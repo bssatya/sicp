70e1476c2665db4f024c27848d151cbfc7d4d1a0
Satya Kumar <satya.kumar@COMVIVA.COM>
2018-05-29 19:18:07 +0530

    solution to excercise 1.3

---
+++ scip_ex_1_3.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(define (get-largest-of-2 a b )
+    (if (> a b) a b)
+)
+
+
+(define (get-smallest-of-2 a b)
+    (if (< a b) a b)
+)
+
+(define (square a)
+    (* a a)
+)
+
+(define (sum a b)
+    (+ a b)
+)
+
+(define (square-sum-larger-2 a b c)
+    (sum (square (get-largest-of-2 a b)) (square (get-largest-of-2 (get-smallest-of-2 a b) c)))
+)
+
+(square-sum-larger-2 10 2 3)
